9aafc46b474d4ec175b7367e0ae949b5c6305de4
KELVIN A OGUNO <kelvox@gmail.com>
2019-11-29 09:05:33 -0700

    commit

--- app/src/main/java/com/kelvingabe/kelvinoguno/prjtacos/controller/BankCardActivity.kt
+++ app/src/main/java/com/kelvingabe/kelvinoguno/prjtacos/controller/BankCardActivity.kt
@@ -2,7 +2,6 @@ package com.kelvingabe.kelvinoguno.prjtacos.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.kelvingabe.kelvinoguno.prjtacos.R
 import kotlinx.android.synthetic.main.activity_bank_card.*
 
@@ -13,10 +12,6 @@ class BankCardActivity : AppCompatActivity() {
         setContentView(R.layout.activity_bank_card)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
